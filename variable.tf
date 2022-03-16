8b5e04cbba07de520b2ab1b572dce3cf52f91e17
saloni <salonisachdeva002@gmail.com>
2022-03-16 20:29:01 +0530

    variable commit

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable"ami"{
+    type=string
+    description = "this value contains ami for instance"
+}
+
+variable"instance_type"{
+    type=string
+    description = "this variable holds the value for instance type "
+}
+
+ variable"name"{
+    type=string
+}
